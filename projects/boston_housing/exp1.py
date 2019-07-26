a6d18538639e0b970dbfad5af901dd39d8dc4297
XJ-Xiaojie <42714017+XJ-Xiaojie@users.noreply.github.com>
2019-07-26 15:10:18 +0800

    Update exp1.py

--- projects/boston_housing/exp1.py
+++ projects/boston_housing/exp1.py
@@ -1,1 +1,15 @@
+#import libraries necessary for this project
+import numpy as np
+import pandas as pd
+from sklearn.model_selection import ShuffleSplit
 
+#Import supplementary visulizations code visuals.py
+import visuals as vs
+#pretty display for notebooks
+%matplotlib inline
+#load the Boston housing dataset
+data = pd.read_csv('housing.csv')
+prices = data['MEDV']
+features = data.drop('MEDV', axis=1)
+#sucess
+print("Boston housing dataset hus {} data points with {} varibales each.".format(*data.shape))
